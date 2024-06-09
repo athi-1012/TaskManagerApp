ea73b63e80b8b5f640a0527d389d55d2ad73225d
athira <athiraasok1012@gmail.com>
2024-06-10 01:07:38 +0530

    Initial commit

---
+++ Server/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://athiraasok1012:mongodbpassword@athi.2hmmwvj.mongodb.net/?retryWrites=true&w=majority&appName=Athi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define Task model
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  dueDate: Date,
+  completed: Boolean
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+// Define Routes
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.json(task);
+});
+
+app.get('/tasks/:id', async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  res.json(task);
+});
+
+app.put('/tasks/:id', async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(task);
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Task deleted' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
